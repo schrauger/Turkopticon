f76be953cd0aec33cbf2308e8426c9649be9d6a2
poolparty <webmaster@istrack.in>
2014-05-24 20:39:49 -0700

    Initial upload to GitHub

---
+++ turkopticon.meta.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name           turkopticon
+// @version        2014.03.07
+// @description    Review requesters on Amazon Mechanical Turk
+// @author         Created by Lilly Irani and Six Silberman, modified by Miku
+// @license        GNU GPL
+// @homepage       http://turkopticon.ucsd.edu
+// @updateURL      https://userscripts.org/scripts/source/166211.meta.js
+// @downloadURL    https://userscripts.org/scripts/source/166211.user.js
+// @include        https://*.mturk.com/mturk/viewhits*
+// @include        https://*.mturk.com/mturk/findhits*
+// @include        https://*.mturk.com/mturk/sorthits*
+// @include        https://*.mturk.com/mturk/searchbar*
+// @include        https://*.mturk.com/mturk/viewsearchbar*
+// @include        https://*.mturk.com/mturk/sortsearchbar*
+// @include        https://*.mturk.com/mturk/preview?*
+// @include        https://*.mturk.com/mturk/statusdetail*
+// @include        https://*.mturk.com/mturk/return*
+// @require        https://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @grant          GM_xmlhttpRequest
+// ==/UserScript==
